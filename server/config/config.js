eafb8816917601fd1010e3a7f5fcf4013427fae6
Diego <monsalve1221@hotmail.com>
2018-08-07 00:20:05 -0500

    Variable de entorno MONGO_URLDB

--- server/config/config.js
+++ server/config/config.js
@@ -15,7 +15,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://cafe-user-123abc:123abc@ds147361.mlab.com:47361/cafe';
+    urlDB = process.env.MONGO_URLDB;
 }
 
 process.env.URLDB = urlDB;
